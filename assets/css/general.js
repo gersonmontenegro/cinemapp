1ba1f97c2e2f5bbeb4be47ea3b7a903a5a7122ed
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-09 18:58:32 +1200

    Making _barHeight a global constant style

--- assets/css/general.js
+++ assets/css/general.js
@@ -2,11 +2,11 @@ import { StyleSheet, Dimensions } from "react-native";
 
 export const screenWidth = Dimensions.get("window").width;
 export const screenHeight = Dimensions.get("window").height;
-
-export const defaultTimeAnimation = 1000;
+export const defaultTimeAnimation = 400;
+export const _barHeight = 75;
 
 const _imageSize = 256;
-const _barHeight = 75;
+
 // this._imageSize = new Animated.Value(this.state.iconSize);
 
 export const SplashStyle = StyleSheet.create({

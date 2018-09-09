ec075719e7e2e518868cd69735646324bcd49910
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-09 18:24:46 +1200

    Adding Main container for the tab menu

---
+++ screens/MainContainer.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import { View, Text } from 'react-native';
+
+class MainContainer extends PureComponent {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+
+}
+
+export default MainContainer;

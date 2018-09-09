5b99533fc287fe53fdd4c03f9e172dc2ec2e1117
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-09 18:24:25 +1200

    Removing unecessary lines, and adding main charger system

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
 import { Platform, StyleSheet, View } from 'react-native';
 import Splash from './screens/Splash';
 import SplashScreen from 'react-native-splash-screen';
+import MainContainer from './screens/Splash';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,34 +13,29 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { splash: true };
+  }
+
+
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: 'gray' }}>
-        <Splash />
+        {this.openCloseSplash()}
       </View>
     );
+  }
+
+  openCloseSplash() {
+    if (this.state.splash) {
+      return (<Splash />);
+    } else {
+      return (<MainContainer />);
+    }
   }
 
   componentDidMount() {
     SplashScreen.hide();
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});

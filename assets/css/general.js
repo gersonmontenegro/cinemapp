dbf177578396566213e930a09437a35b2cc4a3aa
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-09 18:15:31 +1200

    Adding Splash styles

--- assets/css/general.js
+++ assets/css/general.js
@@ -2,3 +2,61 @@ import { StyleSheet, Dimensions } from "react-native";
 
 export const screenWidth = Dimensions.get("window").width;
 export const screenHeight = Dimensions.get("window").height;
+
+const _imageSize = 256;
+const _barHeight = 75;
+// this._imageSize = new Animated.Value(this.state.iconSize);
+
+export const SplashStyle = StyleSheet.create({
+    BwImageContainer: {
+        position: "absolute",
+        justifyContent: "center",
+        width: screenWidth,
+        height: screenHeight
+    },
+    BwImage: {
+        width: _imageSize,
+        height: _imageSize,
+        alignSelf: "center"
+    },
+    ColorImageContainer: {
+        position: "absolute",
+        width: screenWidth,
+        height: screenHeight,
+        zIndex: 10
+    },
+    ColorImage: {
+        alignSelf: "center",
+    },
+    textCinemaContainer: {
+        position: "absolute",
+        width: (screenWidth / 2),
+        justifyContent: 'center',
+        height: _barHeight,
+    },
+    textCinema: {
+        fontFamily: 'ObliviousFont',
+        fontSize: 20,
+        color: 'white',
+        alignSelf: "flex-end",
+    },
+    textAppContainer: {
+        position: "absolute",
+        width: (screenWidth / 2),
+        justifyContent: 'center',
+        marginLeft: (screenWidth / 2),
+        height: _barHeight,
+    },
+    textApp: {
+        fontFamily: 'ObliviousFont',
+        fontSize: 20,
+        color: 'white',
+        alignSelf: "flex-start",
+    },
+    barContainer: {
+        position: "absolute",
+        width: screenWidth,
+        backgroundColor: 'black',
+        opacity: 0.7
+    }
+});

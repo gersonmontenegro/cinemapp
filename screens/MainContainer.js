bd51522c354879ff86a2758339a2970968e6799a
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-10 21:33:51 +1200

    Creating a temporary data with the movie details, and sent them to the movie component

--- screens/MainContainer.js
+++ screens/MainContainer.js
@@ -52,9 +52,32 @@ class MainContainer extends PureComponent {
             { key: '7', name: 'Superboy' },
             { key: '8', name: 'The call' },
         ];
+        let movieDetails =
+        {
+            "vote_count": 7720,
+            "id": 299536,
+            "video": false,
+            "vote_average": 8.3,
+            "title": "Avengers: Infinity War",
+            "popularity": 260.161,
+            "poster_path": "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+            url_img: require('./../assets/img/movie.jpg'),
+            "original_language": "en",
+            "original_title": "Avengers: Infinity War",
+            "genre_ids": [
+                12,
+                878,
+                14,
+                28
+            ],
+            "backdrop_path": "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+            "adult": false,
+            "overview": "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
+            "release_date": "2018-04-25"
+        }
         return (
             <Animated.View style={{ width: screenWidth }}>
-                <MainMovie height={this.mainMovieHeight} />
+                <MainMovie height={this.mainMovieHeight} data={movieDetails} />
                 <Animated.View style={{ width: screenWidth }}>
                     {
                         data.map((item) => (

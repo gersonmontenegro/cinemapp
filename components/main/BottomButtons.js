e588e4656b7dd7c0b678170efc9aa26827708e38
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-11 02:39:15 +1200

    Using the new BottomButton component

--- components/main/BottomButtons.js
+++ components/main/BottomButtons.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { TouchableHighlight, View, Animated } from 'react-native';
 import { screenWidth } from '../../assets/css/general';
+import BottomButton from './BottomButton';
 
 class BottomButtons extends PureComponent {
     constructor(props) {
@@ -9,18 +10,11 @@ class BottomButtons extends PureComponent {
 
     render() {
         return (
-            // <Animated.View style={{ top: -130 }}>
             <Animated.View style={{ top: this.props.top }}>
                 <View style={{ width: screenWidth, flexDirection: "row", height: 30 }}>
-                    <TouchableHighlight style={{ width: (screenWidth / 3), backgroundColor: '#00ff0000', justifyContent: "center", alignItems: "flex-end" }}>
-                        <Animated.Image style={{ width: 30, height: 30 }} source={require('./../../assets/img/heart_empty.png')} />
-                    </TouchableHighlight>
-                    <TouchableHighlight style={{ width: (screenWidth / 3), backgroundColor: '#ff00ff00', justifyContent: "center", alignItems: "center" }}>
-                        <Animated.Image style={{ width: 100, height: 80 }} source={require('./../../assets/img/play.png')} />
-                    </TouchableHighlight>
-                    <TouchableHighlight style={{ width: (screenWidth / 3), backgroundColor: '#00ff0000', justifyContent: "center", alignItems: "flex-start" }}>
-                        <Animated.Image style={{ width: 30, height: 30 }} source={require('./../../assets/img/more_empty.png')} />
-                    </TouchableHighlight>
+                    <BottomButton width={30} height={30} type={0} alignItems="flex-end" />
+                    <BottomButton width={100} height={80} type={1} alignItems="center" />
+                    <BottomButton width={30} height={30} type={2} alignItems="flex-start" />
                 </View>
             </Animated.View>
         );

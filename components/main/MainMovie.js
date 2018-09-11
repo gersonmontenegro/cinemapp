f696eea2628e142c08f81a59fc469904e77fdca1
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-11 15:06:05 +1200

    Adding Pie to the Main Movie component

--- components/main/MainMovie.js
+++ components/main/MainMovie.js
@@ -4,6 +4,7 @@ import { IMAGE_URL, screenWidth } from '../../assets/css/general';
 import Process from './../../providers/Process';
 import TextInfo from './TextInfo';
 import BottomButtons from './BottomButtons';
+import Pie from 'react-native-pie';
 
 class MainMovie extends PureComponent {
     constructor(props) {
@@ -17,12 +18,34 @@ class MainMovie extends PureComponent {
                 <Animated.Image
                     style={{ width: screenWidth, height: this.props.height }}
                     source={{ uri: IMAGE_URL + this.props.data.backdrop_path }} />
-
-                <TextInfo top={-105} fontSize={15} text={this.Process.toUpper(this.props.data.title)} />
-                <TextInfo center={true} top={-118} fontSize={12} text="Action | Fiction | Fantasy" />
-                <TextInfo top={-135} fontSize={12} text={this.Process.cutText(this.props.data.overview, 95)} />
-                <BottomButtons top={-130} />
-
+                <View style={{ top: -280 }}>
+                    <Pie
+                        radius={30}
+                        innerRadius={25}
+                        series={[60]}
+                        colors={['#f00']}
+                        backgroundColor='white'
+                    />
+                    <View style={{
+                        position: 'absolute',
+                        width: 60,
+                        height: 60,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                        <Text style={{
+                            backgroundColor: 'transparent',
+                            color: 'white',
+                            alignSelf: 'center',
+                            fontFamily: 'ObliviousFont',
+                            fontSize: 16,
+                        }}>60%</Text>
+                    </View>
+                </View>
+                <TextInfo top={-165} fontSize={15} text={this.Process.toUpper(this.props.data.title)} />
+                <TextInfo center={true} top={-180} fontSize={12} text="Action | Fiction | Fantasy" />
+                <TextInfo top={-195} fontSize={12} text={this.Process.cutText(this.props.data.overview, 95)} />
+                <BottomButtons top={-190} />
             </Animated.View>
         );
     }

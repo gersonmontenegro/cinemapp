a28bddefa6fba75c96ce8ab251d34afdcfdf221d
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-12 23:53:54 +1200

    Adding the createTables to the Database provider in order to make the tables category, movie, genre, and favorites

--- providers/Database.js
+++ providers/Database.js
@@ -16,8 +16,72 @@ class Database {
         return Database.instance;
     }
 
-    constructor() {
-        console.log("lili????");
+    createTables = () => {
+        return new Promise((resolve, reject) => {
+            var tables = [];
+            var query = `
+            CREATE TABLE IF NOT EXISTS category (
+              create_time timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+              update_time timestamp NULL DEFAULT NULL,
+              active INTEGER NOT NULL DEFAULT 1,
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT DEFAULT NULL,
+              url TEXT DEFAULT NULL
+            );`;
+            tables.push(query);
+            query = `
+            CREATE TABLE IF NOT EXISTS genre (
+              create_time timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+              update_time timestamp NULL DEFAULT NULL,
+              active INTEGER NOT NULL DEFAULT 1,
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT DEFAULT NULL
+            );`;
+            tables.push(query);
+            query = `
+            CREATE TABLE IF NOT EXISTS movie (
+              create_time timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+              update_time timestamp NULL DEFAULT NULL,
+              active INTEGER NOT NULL DEFAULT 1,
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              vote_count INTEGER DEFAULT NULL,
+              vote_average INTEGER DEFAULT NULL,
+              title TEXT DEFAULT NULL,
+              popularity INTEGER DEFAULT NULL,
+              poster_path TEXT DEFAULT NULL,
+              original_language TEXT DEFAULT NULL,
+              original_title TEXT DEFAULT NULL,
+              genre_ids TEXT DEFAULT NULL,
+              backdrop_path TEXT DEFAULT NULL,
+              adult TEXT DEFAULT NULL,
+              overview TEXT DEFAULT NULL,
+              release_date TEXT,
+              idcategory INTEGER
+            );`;
+            tables.push(query);
+            query = `
+            CREATE TABLE IF NOT EXISTS favorite (
+              create_time timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+              update_time timestamp NULL DEFAULT NULL,
+              active INTEGER NOT NULL DEFAULT 1,
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              iduser INTEGER,
+              idmovie INTEGER
+            );`;
+            tables.push(query);
+
+            tables.forEach((q, index) => {
+                this.executeQuery(q).then(
+                    (res) => {
+                        console.log(index + ": RESULT: " + res);
+                        if (index == tables.length - 1) {
+                            resolve("at this time, it's supposed that is finished");
+                        }
+                    }
+                );
+            });
+        });
+    }
     }
 
 

c39130550570fd7bfb3ecff483e548a9837c6123
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-15 20:31:10 +1200

    Removing console message

--- components/mainmovie/BottomButton.js
+++ components/mainmovie/BottomButton.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { TouchableHighlight, Animated } from 'react-native';
-import { screenWidth, HEART_FULL_ICON } from '../../assets/css/general';
+import { screenWidth, HEART_FULL_ICON, backColorToRemoveWink } from '../../assets/css/general';
 import Process from './../../providers/Process';
 
 class BottomButton extends PureComponent {
@@ -27,15 +27,13 @@ class BottomButton extends PureComponent {
         if (this.props.type == 0) {
             this.setState({ source: HEART_FULL_ICON });
         } else if (this.props.type == 1) {
-            console.log("NOW LET MY BEET CONTROL YOUR BODY!!");
-
             this.props.changeMainMoviePosition(-screenWidth);
         }
     }
 
     render() {
         return (
-            <TouchableHighlight underlayColor={'#ffffff00'} onPress={() => this.onPressButton()} style={{ width: (screenWidth / 3), backgroundColor: '#ffffff00', justifyContent: "center", alignItems: this.props.alignItems }}>
+            <TouchableHighlight underlayColor={backColorToRemoveWink} onPress={() => this.onPressButton()} style={{ width: (screenWidth / 3), backgroundColor: backColorToRemoveWink, justifyContent: "center", alignItems: this.props.alignItems }}>
                 <Animated.Image
                     style={{ width: this.props.width, height: this.props.height }}
                     source={this.state.source} />

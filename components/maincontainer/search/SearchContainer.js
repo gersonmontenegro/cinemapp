70ecc1d998c1a4db51eebc6c8619096467016b32
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-16 21:47:58 +1200

    Implementing search offline

--- components/maincontainer/search/SearchContainer.js
+++ components/maincontainer/search/SearchContainer.js
@@ -1,8 +1,10 @@
 import React, { PureComponent } from 'react';
 import { Animated, TextInput } from 'react-native'
-import { screenWidth, SEARCH_MORE_ICON } from '../../../assets/css/general';
+import { screenWidth, SEARCH_MORE_ICON, finalHeighMainMovie, initHeighMainMovie } from '../../../assets/css/general';
 import BasicButton from '../../general/BasicButton';
 import Actions from './../../../providers/Actions';
+import MovieList from '../../categories/MovieList';
+import Process from '../../../providers/Process';
 
 class SearchContainer extends PureComponent {
     constructor(props) {
@@ -14,27 +16,42 @@ class SearchContainer extends PureComponent {
 
     creatingSingletonGroup() {
         this.Actions = Actions.getInstance();
+        this.Process = Process.getInstance();
     }
 
     bindingFunctions() {
         this.onPressMore = this.onPressMore.bind(this);
+        this.onChangeText = this.onChangeText.bind(this);
     }
 
     settingState() {
-        this.state = { height: this.props.height }
+        this.state = {
+            height: this.props.height,
+            searchText: '',
+            searchResults: {}
+        }
     }
 
     onPressMore = () => {
-        this.Actions.changeVariable(this.props.height, 200, 0).start();
+        if (this.props.height.__getValue() < finalHeighMainMovie) {
+            this.Actions.changeVariable(this.props.height, finalHeighMainMovie, 0).start();
+        } else {
+            this.Actions.changeVariable(this.props.height, initHeighMainMovie, 0).start();
+        }
     }
 
-    startSearch() {
-
+    onChangeText = (text) => {
+        this.setState({ text: text });
+        if (text.length >= 3) {
+            this.Process.searchMovie(text).then((data) => {
+                this.setState({ searchResults: data });
+            });
+        }
     }
 
     render() {
         return (
-            <Animated.View style={{ width: screenWidth, height: this.state.height, backgroundColor: 'gray' }}>
+            <Animated.View style={{ width: screenWidth, height: this.state.height, backgroundColor: 'lightgray' }}>
                 <Animated.View style={{ flexDirection: 'row', height: 30, width: screenWidth }}>
                     <TextInput
                         autoCapitalize="characters"
@@ -42,8 +59,10 @@ class SearchContainer extends PureComponent {
                         placeHolder="Tab to start the search"
                         placeholderTextColor="red"
                         clearButtonMode="always"
+                        value={this.state.searchText}
+                        onChangeText={this.onChangeText}
                         style={{
-                            fontFamily: 'ObliviousFont',
+                            fontFamily: 'JosefinSans',
                             width: screenWidth - 30,
                             marginLeft: 2,
                             height: 30,
@@ -51,15 +70,13 @@ class SearchContainer extends PureComponent {
                             borderColor: 'gray',
                             borderWidth: 1
                         }} />
-                    {/* <BasicButton onPressButton={} buttonStyle={{ marginLeft: 5, justifyContent: 'center' }} width={20} height={20} icon={SEARCH_ICON} /> */}
                     <BasicButton onPressButton={this.onPressMore}
                         buttonStyle={{
-                            marginLeft: 5,
                             justifyContent: 'center',
-                            backgroundColor: 'gray'
+                            backgroundColor: 'lightgray'
                         }} width={25} height={20} icon={SEARCH_MORE_ICON} />
                 </Animated.View>
-
+                <MovieList height={this.props.height} search="xxx" searchResults={this.state.searchResults} />
             </Animated.View>
         );
     }

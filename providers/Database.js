bd0fcfd6f1d7ac6e255895442912bf6f70fbf2d0
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-16 17:19:12 +1200

    Saving movie data

--- providers/Database.js
+++ providers/Database.js
@@ -1,7 +1,11 @@
 let SQLite = require('react-native-sqlite-storage');
+import Process from './Process';
+import { IMAGE_URL } from './Data';
 
 class Database {
     _db = null;
+    _Process = Process.getInstance();
+
     static instance = null;
 
     static createInstance() {
@@ -88,11 +92,76 @@ class Database {
         });
     }
 
+    updateMovie(item) {
+        // console.log("updating movie...", item.id);
+        let query = `select * from movie where id = ${item.id}`;
+        this.executeQuery(query).then(
+            (data) => {
+                if (parseInt(data.rows.length) > 0) {
+                    var item = data.rows.item(0);
+                    // console.log("UPDATING:", item.id, this._Process.existsImageBackground(item.backdrop_path, item));
+                    this.getBase64Img(item.id, this._Process.existsImageBackground(item.backdrop_path, item));
+                }
+            }
+        );
+    }
+
+    insertMovie(item, category) {
+        item.overview = this._Process.removingApostrophe(item.overview);
+        item.title = this._Process.removingApostrophe(item.title);
+        item.original_title = this._Process.removingApostrophe(item.original_title);
+        let query = `insert into movie (
+            id,vote_count,vote_average,title,
+            popularity,poster_path,original_language,original_title,
+            genre_ids,backdrop_path,adult,overview,
+            release_date,idcategory
+            ) values(
+                ${item.id}, ${item.vote_count}, ${item.vote_average}, '${item.title}',
+                '${item.popularity}', '${item.poster_path}', '${item.original_language}', '${item.original_title}',
+                '${item.genre_ids}', '${item.backdrop_path}', '${item.adult}', '${item.overview}',
+                '${item.release_date}', ${category}
+            )`;
+        this.executeQuery(query).then(
+            (data) => {
+                console.log("INSERTED:", data);
+                this.getBase64Img(item.id, this._Process.existsImageBackground(item.backdrop_path, item));
+            }
+        );
+    }
+
+    getBase64Img(id, name) {
+        // ImgToBase64.getBase64String(IMAGE_URL + name)
+        //     .then(base64String => this.saveBase64Img(base64String))
+        //     .catch(err => this.saveBase64Img(err));
+    }
+
+    saveBase64Img(base64img) {
+        console.log("ResultBase64", base64img);
+
+    }
+
+    saveMovies(movies, category) {
+        movies.forEach((item, index) => {
+            let idQuery = `select count(1) cantidad from movie where id = ${item.id}`;
+            this.executeQuery(idQuery).then(
+                (data) => {
+                    if (parseInt(data.rows.item(0)["cantidad"]) > 0) {
+                        this.updateMovie(item);
+                    } else {
+                        this.insertMovie(item, category);
+                    }
+                }
+            );
+        });
+    }
+
     executeQuery = (query) => {
         return new Promise((resolve, reject) => {
             this._db.transaction((tx) => {
                 tx.executeSql(query, [], (tx, results) => {
                     resolve(results);
+                }, (err) => {
+                    // console.log("Error: ", err, "With QUERY:", query);
                 });
             });
         });

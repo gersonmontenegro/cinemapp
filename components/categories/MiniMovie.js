b25fd35651fcb9763b03729333f7723f7d7374d0
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-18 01:35:10 +1200

    Implementing new versions of getImgPath, and truncateTitle methods

--- components/categories/MiniMovie.js
+++ components/categories/MiniMovie.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { Animated, Image, TouchableHighlight } from 'react-native';
-import { MiniMovieStyle, finalHeighMainMovie } from './../../assets/css/general';
+import { MiniMovieStyle, finalHeighMainMovie, veryLightGray } from './../../assets/css/general';
 import Actions from './../../providers/Actions';
 import { IMAGE_URL } from './../../providers/Data';
 import Process from '../../providers/Process';
@@ -34,12 +34,12 @@ class MiniMovie extends PureComponent {
                 <Animated.View style={{ flexDirection: 'column', alignItems: 'center' }}>
                     <Animated.View style={MiniMovieStyle.imageContainer}>
                         <Image style={MiniMovieStyle.imageStyle}
-                            source={{ uri: IMAGE_URL + this.props.item.poster_path }}
+                            source={{ uri: this.Process.getImgPath(this.props.item.poster_path, this.props.item) }}
                         />
                     </Animated.View>
                     <Animated.View >
-                        <Animated.Text style={MiniMovieStyle.titleStyle}>
-                            {this.Process.truncateTitle(this.props.item.title)}
+                        <Animated.Text style={[MiniMovieStyle.titleStyle, { color: this.props.textColor != undefined ? this.props.textColor : 'black' }]}>
+                            {this.Process.truncateTitle(this.props.item.title, this.props.item)}
                         </Animated.Text>
                     </Animated.View>
                 </Animated.View>
